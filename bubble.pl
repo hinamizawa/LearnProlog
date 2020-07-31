eac73ba6cacfe07be5c6784936b251fc20ce76ea
Junyu Lu <jy.lu@outlook.com>
2020-07-31 17:07:40 +0800

    format

--- bubble.pl
+++ bubble.pl
@@ -13,3 +13,21 @@ swap(Rel,[A,B|List],[B,A|List]) :-
 
 swap(Rel,[A|List],[A|NewList]) :-
     swap(Rel,List, NewList).
+
+quicksort(_,[],[]).
+
+quicksort(Rel,[Head|Tail],Sorted):-
+    split(Rel, Head, Tail,Left,Right),
+    quicksort(Rel,Left,SortedLeft),
+    quicksort(Rel,Right,SortedRight),
+    append(SortedLeft,[Head|SortedRight],Sorted).
+
+split(_, _, [], [], []).
+
+split(Rel, Middle, [Head|Tail], [Head|Left], Right) :-
+    check(Rel, Head, Middle), 
+    !,
+    split(Rel, Middle, Tail, Left, Right).
+
+split(Rel, Middle, [Head|Tail], Left, [Head|Right]) :-
+    split(Rel, Middle, Tail, Left, Right).
