890ff6375250a728e5e120d9b85921d2c7ab17f6
hinamizawa <jy.lu@outlook.com>
2020-08-01 22:11:40 +0800

    continued

--- 99Problems/prolog_lists.pl
+++ 99Problems/prolog_lists.pl
@@ -38,3 +38,26 @@ reverse([X|Xs], Rs, Ys, [_|Bound]) :-
 palindrome(X) :- reverse(X,X).
 
 % 1.07 (**) Flatten a nested list structure.
+my_flatten([H],X) :- 
+    (is_list(H) -> !, my_flatten(H,X); 
+                   !, append([H],[],X)).
+
+my_flatten([H|T],X) :-
+    (is_list(H) -> !,append(Y,Z,X), my_flatten(H,Y), my_flatten(T,Z); 
+                   !,append([H],Y,X), my_flatten(T,Y)).
+/*
+flatten(List, FlatList) :-
+    flatten(List, [], FlatList0),
+    !,
+    FlatList = FlatList0.
+
+flatten(Var, Tl, [Var|Tl]) :-
+    var(Var),
+    !.
+flatten([], Tl, Tl) :- !.
+flatten([Hd|Tl], Tail, List) :-
+    !,
+    flatten(Hd, FlatHeadTail, List),
+    flatten(Tl, Tail, FlatHeadTail).
+flatten(NonList, Tl, [NonList|Tl]).
+*/
